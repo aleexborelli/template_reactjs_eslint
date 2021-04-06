2607342e5dd13249fea098746cb3bf41fd65067d
Alex Borelli <alex.sander.borelli@outlook.com.br>
2021-04-06 11:39:10 -0300

    Template Projeto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return <h1>Hello World!</h1>;
+}
+
+export default App;
